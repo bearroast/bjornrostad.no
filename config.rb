5e3041b6e5a378a4d6cb63c5fd320c4d42eb4ffd
Bjørn Rostad <hi@bjornrostad.no>
2016-09-14 10:25:47 -0700

    cleanup

--- config.rb
+++ config.rb
@@ -9,4 +9,9 @@ end
 configure :build do
   activate :minify_css
   activate :minify_javascript
+  activate :asset_hash
 end
+
+activate :directory_indexes
+activate :relative_assets
+set :relative_links, true
