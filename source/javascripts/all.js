906ad51cc008f34b3035e802332e4a60875658b7
Bjørn Rostad <hi@bjornrostad.no>
2016-10-10 15:05:17 -0700

    nav kind of working

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,16 @@
 //= require vendor/jquery
-//= require vendor/jquery.smoothState
 
+// Initiate fullscreen nav
 $(function() {
-  $('#main').smoothState();
+    $('.toggle-nav').click(function() {
+        if ($('body').hasClass('show-nav')) {
+            $('body').removeClass('show-nav').addClass('hide-nav');
+            setTimeout(function() {
+                $('body').removeClass('hide-nav');
+            }, 500);
+        } else {
+            $('body').removeClass('hide-nav').addClass('show-nav');
+        }
+        return false;
+    });
 });
