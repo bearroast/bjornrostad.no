13bea5741fad341bb40e85f4294cccd6bb8166d9
Bjørn Rostad <hi@bjornrostad.no>
2017-04-18 12:31:23 -0700

    clean up

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,15 +1,1 @@
 //= require vendor/jquery
-
-$(function() {
-    $('.toggle-nav').click(function() {
-        if ($('body').hasClass('show-nav')) {
-            $('body').removeClass('show-nav').addClass('hide-nav');
-            setTimeout(function() {
-                $('body').removeClass('hide-nav');
-            }, 1500);
-        } else {
-            $('body').removeClass('hide-nav').addClass('show-nav');
-        }
-        return false;
-    });
-});
